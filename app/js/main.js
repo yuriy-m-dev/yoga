41e6241808abc9ce3a011f8d24680ebca7e4bd5d
yuriy-m-dev <fath66426@gmail.com>
2020-02-09 19:56:22 +0200

    added a 'travel' section

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.travel__slider').slick({
+        slidesToShow: 3,
+        centerMode: true,
+        variableWidth: true,
+        arrows: false,
+        autoplay: true
+    });
+});
