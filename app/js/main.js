c8a559f27ba7606d2361c28dc3cebadb51b8c256
yuriy-m-dev <fath66426@gmail.com>
2020-02-10 14:55:10 +0200

    added section program

--- app/js/main.js
+++ app/js/main.js
@@ -9,4 +9,15 @@ $(function() {
        autoplay: true,
        autoplayTimeout: 4000
    });
+
+   $('.program__slider').owlCarousel({
+    loop: true,
+    items: 3,
+    autoWidth: true,
+    margin: 30,
+    center: true,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 4000
+});
 });
